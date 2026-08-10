baaf3eb344b1d930ab138921de0feeca9b60181b
Rafael Ibarra <ripdisweb@gmail.com>
2026-08-10 14:29:23 -0700

    chore: remove debug endpoints, login working

--- src/pages/api/ping.ts
+++
@@ -1,8 +0,0 @@
-export const prerender = false;
-
-export async function POST() {
-  return new Response(JSON.stringify({ ok: true }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
